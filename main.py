7be0060cb78b28166c86232578297cb88217004c
Hobrus <bhobrus@gmail.com>
2023-04-23 12:40:03 +0300

    Fix all

--- main.py
+++ main.py
@@ -5,4 +5,23 @@ from decorators import *
 DATA = {5000: 0, 2000: 0, 1000: 0, 500: 0}
 
 welcome()
+
+
+@dec_choice
+def choice_input():
+    return choice()
+
+
+def main():
+    while True:
+        c = choice_input()
+        if c == 1:
+            choice_1(DATA)
+        elif c == 2:
+            atm_replenishment(DATA)
+        else:
+            goodbye()
+            break
+
+
 main()
